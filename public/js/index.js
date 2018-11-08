6f26679fa6859fd0bb501a13995851e4cb3ee9ac
mikael1000 <mikael.johansson.1000@live.se>
2018-11-08 13:39:46 +0100

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io()
 
 socket.on('connect', function () {
     console.log('Connected to server')
-
-    socket.emit('createMessage', {
-        from: 'Mike',
-        text: 'Hey! This is text'
-    })
 })
 
 socket.on('disconnect', function () {
